585b8be98dd70c3b4ef56c9ca3164a7dedc39bd2
fuminluo <474985045@qq.com>
2018-05-21 23:22:49 +0800

    集成redis 失效监听 “__keyevent@0__:expired”

---
+++ src/main/java/priv/rabbit/vio/config/RedisKeyExpiredListener.java
@@ -0,0 +1,27 @@
+package priv.rabbit.vio.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.concurrent.CountDownLatch;
+
+
+public class RedisKeyExpiredListener {
+    private static Logger LOG = LoggerFactory.getLogger(RedisKeyExpiredListener.class);
+
+    private CountDownLatch latch;
+
+    @Autowired
+    public RedisKeyExpiredListener(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    //jie
+    public void receiveMessage(String message) {
+        LOG.info(new Date().toString() + "::" + "message::" + message);
+        latch.countDown();
+    }
+
+}
--- src/main/java/priv/rabbit/vio/controller/UserController.java
+++ src/main/java/priv/rabbit/vio/controller/UserController.java
@@ -2,6 +2,7 @@ package priv.rabbit.vio.controller;
 
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import priv.rabbit.vio.common.ResultInfo;
@@ -9,6 +10,7 @@ import priv.rabbit.vio.dto.user.LoginRequest;
 import priv.rabbit.vio.service.UserService;
 
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class UserController {
@@ -16,9 +18,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @ApiOperation(value = "获取用户列表", notes = "")
     @GetMapping("api/app/v1/user/find/list")
     public Object findUserList() {
+        stringRedisTemplate.opsForValue().set("sprinboot-redis-messaage", "message", 10, TimeUnit.SECONDS);
         return new ResultInfo(ResultInfo.SUCCESS, ResultInfo.MSG_SUCCESS, "123456");
     }
 
@@ -31,22 +37,23 @@ public class UserController {
 
     @PostMapping("api/app/v1/user/register")
     public Object register(@Valid @RequestBody LoginRequest request, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()){
-            return new ResultInfo(ResultInfo.FAILURE, ResultInfo.MSG_FAILURE,bindingResult.getFieldError().getDefaultMessage());
+        if (bindingResult.hasErrors()) {
+            return new ResultInfo(ResultInfo.FAILURE, ResultInfo.MSG_FAILURE, bindingResult.getFieldError().getDefaultMessage());
         }
         return userService.register(request);
     }
 
     /**
      * 登录
+     *
      * @param request（用户名和密码）
      * @param bindingResult
      * @return
      */
     @PostMapping("api/app/v1/user/login")
     public Object login(@Valid @RequestBody LoginRequest request, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()){
-            return new ResultInfo(ResultInfo.FAILURE, ResultInfo.MSG_FAILURE,bindingResult.getFieldError().getDefaultMessage());
+        if (bindingResult.hasErrors()) {
+            return new ResultInfo(ResultInfo.FAILURE, ResultInfo.MSG_FAILURE, bindingResult.getFieldError().getDefaultMessage());
         }
         return userService.login(request);
     }
