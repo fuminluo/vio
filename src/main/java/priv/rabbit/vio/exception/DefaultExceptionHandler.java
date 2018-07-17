b322f9c0096d251d0592d1899c21dddfeb594432
罗富民 <474985045@qq.com>
2018-07-17 11:12:33 +0800

    添加无权限异常捕获

--- src/main/java/priv/rabbit/vio/exception/DefaultExceptionHandler.java
+++ src/main/java/priv/rabbit/vio/exception/DefaultExceptionHandler.java
@@ -1,6 +1,8 @@
 package priv.rabbit.vio.exception;
 
+import com.alibaba.fastjson.JSON;
 import org.apache.shiro.authc.AccountException;
+import org.apache.shiro.authz.UnauthorizedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -29,6 +31,26 @@ public class DefaultExceptionHandler {
     public ResultInfo handleShiroException(AccountException ex) {
         ex.printStackTrace();
         return new ResultInfo(ResultInfo.FAILURE, ex.getMessage());
+    }
+
+    /**
+     * 捕获无权限异常
+     *
+     * @param e
+     * @return
+     * @date 2018年7月17日
+     * @author LiMaoDa
+     */
+    @ExceptionHandler(value = UnauthorizedException.class)
+    @ResponseBody
+    private ResultInfo<Object> unauthorizedExceptionHandler(Exception e) {
+        LOG.error("》》》 UnauthorizedException ：捕获无权限异常");
+        if (LOG.isDebugEnabled()) {
+            e.printStackTrace();
+            LOG.error(JSON.toJSONString(e.getStackTrace()));
+        }
+        ResultInfo<Object> resultInfo = new ResultInfo<Object>(ResultInfo.FAILURE, "您没有权限访问！");
+        return resultInfo;
     }
 
 }
